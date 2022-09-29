71c266dd089b4fec97ca0404be641e511f9de187
avarga-byte <78478893+avarga-byte@users.noreply.github.com>
2022-09-29 15:44:08 +0900

    udpate

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -33,7 +33,8 @@ function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const login = async () => {
+    const login = async (e) => {
+        e.preventDefault()
         let formdata = new FormData()
         formdata.append("email", email)
         formdata.append("password", password)
@@ -66,7 +67,7 @@ function Login() {
             <div className='w-96 h-48 rounded-md bg-white drop-shadow-2xl flex flex-col justify-center items-center p-4'>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' type={"email"} className='border w-5/6 p-2 rounded-md mt-2' />
                 <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' type={"password"} className='border w-5/6 p-2 rounded-md mt-2' />
-                <button onClick={() => login()} className='border w-5/6 p-2 rounded-md mt-2'>
+                <button onClick={(e) => login(e)} className='border w-5/6 p-2 rounded-md mt-2'>
                     Login
                 </button>
             </div>
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,7 @@
 
-export const url = "http://65.22.106.255:3000/api"
-export const cdnURL = "http://65.22.106.255:3000"
-export const webURL = "http://65.22.106.255:3000"
+export const url = "http://165.22.106.255:3000/api"
+export const cdnURL = "http://165.22.106.255:3000"
+export const webURL = "http://165.22.106.255:3000"
+
+
+//165.22.106.255

9ea0af0a442e2003242fe10619c878757944e3c9
avarga-byte <78478893+avarga-byte@users.noreply.github.com>
2022-09-29 15:14:24 +0900

    update

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 
-export const url = "http://68.183.180.219:3000/api"
-export const cdnURL = "http://68.183.180.219:3000"
-export const webURL = "http://68.183.180.219:3000"
+export const url = "http://65.22.106.255:3000/api"
+export const cdnURL = "http://65.22.106.255:3000"
+export const webURL = "http://65.22.106.255:3000"
